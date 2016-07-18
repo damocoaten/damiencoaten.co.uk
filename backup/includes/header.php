05d13d746a106adc5259ec6d743d4eae7bcd9661
Damo Coaten <damocoaten@hotmail.com>
2016-07-18 15:34:57 +0100

    backup remove

--- backup/includes/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-        <a href="/" title="Damien Coaten | Graphic Design Nottingham"><img class="mainlogo" src="images/damien-logo.png" alt="Web Design Nottingham | Graphic Design Nottingham"/></a>
-        <div class="contact-header">
-            <span><i class="fa fa-envelope-o"></i> damocoaten@hotmail.com</span>
-            <span><i class="fa fa-mobile"></i> 07557383926</span>
-        </div>
-    </header>
